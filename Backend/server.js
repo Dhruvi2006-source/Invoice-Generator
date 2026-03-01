fca59cc8c9509d3d7a6a031e747fc91093d04538
2006Dhruvi <200dhruvi@gmail.com>
2026-03-01 22:11:46 +0530

    Auth is ready

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Init Middleware
+app.use(express.json());
+
+// Enable CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    optionsSuccessStatus: 200,
+  })
+);
+
+// Define Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
